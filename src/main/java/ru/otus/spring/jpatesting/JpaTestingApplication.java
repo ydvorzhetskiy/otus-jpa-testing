c78541919c35ea1b4221ac47bcbf58846e5847ad
ydvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-08 14:53:23 +0300

    Example has been added

---
+++ src/main/java/ru/otus/spring/jpatesting/JpaTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.jpatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/spring/jpatesting/JpaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.jpatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
