760700a9704fed83eb5f098328e2761c0b227df3
ydvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-09 12:27:20 +0300

    First fix

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table person
 (
-    id          bigint,
-    person_name varchar(255)
+    id   bigint,
+    name varchar(255)
 );
 
 create sequence person_seq start with 1 increment by 1;
--- src/test/java/ru/otus/spring/jpatesting/model/PersonTest.java
+++ src/test/java/ru/otus/spring/jpatesting/model/PersonTest.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,6 +14,7 @@ class PersonTest {
     @Autowired
     private TestEntityManager em;
 
+    @Commit
     @Test
     void saveAndGet() {
         Person original = new Person("Ivan");
